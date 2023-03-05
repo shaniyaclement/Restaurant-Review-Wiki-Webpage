0a6ad12fefa634c891ab1ba3ab5e9a2546d99da3
Dagmawi Workineh Yemesgen <dagi_works@techexchange.in>
2023-03-05 20:48:56 +0000

    Update pages.py

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, make_response, Response
 
 def make_endpoints(app, backend):
     @app.route("/")
@@ -21,8 +21,9 @@ def make_endpoints(app, backend):
         return render_template('login.html')
     
     @app.route('/upload')  
-    def upload():  
-        return render_template("upload.html")    
+    def upload():
+        username = request.args.get('username', default="")  
+        return render_template("upload.html", username=username)    
 
     @app.route("/authenticate", methods=["POST"])
     def authenticate():
@@ -48,21 +49,32 @@ def make_endpoints(app, backend):
             return render_template('sign_up.html', error=error_message, show_popup=True)
 
     @app.route("/logout")
-    def dashboard():
-        return render_template('main.html', username='')
+    def logout():
+        #return render_template('main.html', username='')
+        res = make_response(render_template('main.html', username=''))
+        res.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        return res
 
     @app.route('/authenticate_upload', methods = ['POST'])  
     def authenticate_upload():  
-        if request.method == 'POST':
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)  
-            file = request.files['file']
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            file.save(file.filename)
-            return home()
+        username = request.args.get('username', default="") 
+        uploaded_file = request.files['file']
+        if not uploaded_file.filename.endswith('.txt'):
+            return render_template('upload.html', error="You can Only upload .txt files!", show_popup=True, username=username)
+        file_contents = uploaded_file.read()
+        # check if file is empty
+        try:
+            decoded_contents = file_contents.decode('utf-8')
+        except UnicodeDecodeError:
+            return render_template('upload.html', error="File is not in UTF-8 encoding!", show_popup=True, username=username)
+        if not decoded_contents.strip():
+            return render_template('upload.html', error="File is empty!", show_popup=True, username=username)
+        f_name = request.form['upload']
+        if len(f_name)<1:
+            return render_template('upload.html', error="Please enter a name for the submission!", show_popup=True, username=username)
+        backend.upload(file_contents, f_name)
+        return render_template('upload.html', error="File uploaded successfully!", show_popup=True, username=username)  # not an error, simply using that param for a pop-message
+        
     @app.route('/pages')
     def pages():
         username = request.args.get('username', default="")
